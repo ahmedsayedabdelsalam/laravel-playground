66da9d65d96c6dd1aa47c38b74955d035ea28e25
ahmed sayed abdelsalam <ahmed.sayed@code95.info>
2019-03-14 15:29:40 +0200

    add twilio

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,27 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Twilio\Rest\Client;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('sendmessage', function() {
+    $sid    = env("TWILIO_ACCOUNT_SID");
+    $token  = env("TWILIO_AUTH_TOKEN");
+    $twilio = new Client($sid, $token);
+
+    $codes = ["chocolate", "vanilla", "strawberry", "mint-chocolate-chip", "cookies-n-cream"];
+
+    $message = $twilio->messages
+        ->create("whatsapp:".env("MY_WHATSAPP_NUMBER"),
+            [
+                "body" => "Your ice-cream code is ".$codes[rand(0,4)],
+                "from" => "whatsapp:".env("TWILIO_WHATSAPP_NUMBER")
+            ]
+        );
+
+    print($message->sid);
 });
