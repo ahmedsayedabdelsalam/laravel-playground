7d257b272f4831a11c319a149cf24aabd83a4dc6
ahmed sayed abdelsalam <ahmed.sayed@code95.info>
2019-06-05 04:24:02 +0200

    add laravel websockets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Laravel</title>
 
@@ -95,5 +96,13 @@
                 </div>
             </div>
         </div>
+
+        <script src="{{ asset('js/app.js') }}"></script>
+	<script>
+            Echo.channel('home')
+                .listen('NewMessage', (e) => {
+                    console.log(e.message)
+                })
+        </script>
     </body>
 </html>
