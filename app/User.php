2c59f4f5636f19b8da2cff081130eb2a8ab72256
Ahmed Sayed Abdelsalam <e.eng.ahmedsayed@gmail.com>
2019-11-11 09:19:43 +0200

    update

--- app/User.php
+++ app/User.php
@@ -28,6 +28,11 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+
     public function posts() {
         return $this->hasMany(Post::class);
     }
