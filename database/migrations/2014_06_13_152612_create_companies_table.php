ff5e4fb1ccadc7dff13e46fc8ba6750191f846c2
ahmed sayed abdelsalam <ahmed.sayed@code95.info>
2020-06-18 03:25:45 +0200

    optimize regex with new normalized virtual column indexd that represent the regex

--- database/migrations/2014_06_13_152612_create_companies_table.php
+++ database/migrations/2014_06_13_152612_create_companies_table.php
@@ -16,6 +16,7 @@ class CreateCompaniesTable extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('name_normalized')->virtualAs("regexp_replace(name, '[^A-Za-z0-9]', '')")->index();
             $table->timestamps();
         });
     }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('name_normalized')->virtualAs("regexp_replace(name, '[^A-Za-z0-9]', '')")->index();
             $table->string('email')->unique();
+            $table->string('email_normalized')->virtualAs("regexp_replace(email, '[^A-Za-z0-9]', '')")->index();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->foreignId('company_id')->constrained()->onDelete('cascade');
